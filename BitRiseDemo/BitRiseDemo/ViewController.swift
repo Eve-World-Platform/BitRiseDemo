e8ce821d6139bbaf16734c6f6fdf149502e799e1
RAC IT SOLUTIONS PVT LTD <racitsolutionspvtltd@RACs-MacBook-Pro.local>
2022-02-07 12:28:51 +0530

    New project created

---
+++ BitRiseDemo/BitRiseDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BitriseDemo
+//
+//  Created by RAC IT SOLUTIONS PVT LTD on 07/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ BitRiseDemo/BitRiseDemoUITests/BitRiseDemoUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  BitriseDemoUITestsLaunchTests.swift
+//  BitriseDemoUITests
+//
+//  Created by RAC IT SOLUTIONS PVT LTD on 07/02/22.
+//
+
+import XCTest
+
+class BitriseDemoUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
